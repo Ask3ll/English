b2b2a50f1360e853ee5178640280182ea1f07524
Ask3ll <maxmolnia@bk.ru>
2024-12-24 23:05:20 +0300

    update

--- database.py
+++ database.py
@@ -3,6 +3,7 @@ class Database:
     def __init__(self):
         self.conn = sqlite3.connect("database.db", check_same_thread=False)
         self.cursor = self.conn.cursor()
+        self.create_table()
     def exec(self, wte, commit=False):
         self.cursor.execute(wte)
         if commit:
@@ -14,9 +15,9 @@ class Database:
         return self.cursor.fetchall()
     def one(self):
         return self.cursor.fetchone()
-    def create_table(self, name):
-        self.exec(f"CREATE TABLE {name} (id INTEGER PRIMARY KEY AUTOINCREMENT, telegram_id INTEGER, correct INTEGER, incorrect INTEGER, total INTEGER)")
-
+    def create_table(self):
+        self.exec("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, telegram_id INTEGER, correct INTEGER, incorrect INTEGER, total INTEGER)")
+        self.exec("CREATE TABLE IF NOT EXISTS questions (id INTEGER PRIMARY KEY AUTOINCREMENT, telegram_id INTEGER, answer TEXT, translate TEXT)")
 if __name__ == "__main__":
     db = Database()
     db.exec(f"DELETE FROM questions", True)
